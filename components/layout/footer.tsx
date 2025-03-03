8644f86e85af2a192804060ad04de43a02065378
zufichris <zufichris@gmail.com>
2025-03-03 18:37:51 +0400

    Added Layout footer.tsx component

---
+++ components/layout/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='w-full text-center  bottom-0 py-4 bg-background rounded-t-md mt-4'>copyright &copy; {new Date().getFullYear()}</div>
+  )
+}
