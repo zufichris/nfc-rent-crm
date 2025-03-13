9dbacf4803e655b8e06f5f54697bd41bbc5529a8
zufichris <zufichris@gmail.com>
2025-03-13 11:20:58 +0400

    Updated next.config in ./. for improvements

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,20 @@ import { NextConfig } from "next"
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   images: {
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
+  // experimental: {
+  //   webpackBuildWorker: true,
+  //   parallelServerBuildTraces: true,
+  //   parallelServerCompiles: true,
+  // },
 }
 
 const withNextIntl = createNextIntlPlugin();
