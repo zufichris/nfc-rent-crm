a73474c6855489b4b7030cc07f83c4b9e1ac1582
zufichris <zufichris@gmail.com>
2025-03-14 17:16:18 +0400

    feat: add app/[locale]/payments/

---
+++ app/[locale]/payments/transactions/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+          <StatCard
+            title='Total'
+            subtitle='Total Number Of Brands'
+            icon={<Package />}
+            variant='info'
+            value={total}
+          />
+          <StatCard
+            title='Inactive'
+            subtitle='Brands with Verified Info'
+            icon={<ShieldBan />}
+            variant='secondary'
+            value={total - (activeCount ?? 0)}
+          />
+          <StatCard
+            title='Active'
+            subtitle='Active Brands'
+            icon={<Activity />}
+            variant='success'
+            value={activeCount}
+          />
+          <StatCard
+            title='Deleted'
+            subtitle='Number of Deleted Brands'
+            icon={<Trash2 />}
+            variant="destructive"
+            value={deletedCount}
+          />
+        </div>
+        <PaymentListTable activeFilters={search} limit={limit} page={page} total={total} brands={brands} /> */}
+      </>
+    )
+}
+
+export default page
