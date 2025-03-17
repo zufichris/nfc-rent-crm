de7bfb9df91d835aa71094449afcfa9347a3810f
zufichris <zufichris@gmail.com>
2025-03-17 10:49:24 +0400

    feat: add app/[locale]/loading.tsx

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,10 @@
+import { TableSkeleton } from '@/components/misc/table/table-skeleton'
+import React from 'react'
+
+const TablePageLoader = () => {
+    return (
+        <TableSkeleton />
+    )
+}
+
+export default TablePageLoader
