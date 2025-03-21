8c758f1130639458f7c62eb7ff3e4b00ec78903f
zufichris <zufichris@gmail.com>
2025-03-21 20:13:15 +0400

    M lib/actions/auth.ts M lib/services/auth.ts M types/shared.ts M utils/functions.ts?? components/auth/

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,10 @@
-export const env={
-    apiUrl:"http://api.nfc.com",
-}
+import z from "zod"
+export const config = z.object({
+    apiUrl: z.string({
+        message: "apiUrl is required",
+    }),
+})
+
+export const env = config.parse({
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+})
