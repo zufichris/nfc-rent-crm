a9df238a2f5d1fa2c09c59c5fcef16297adf1782
zufichris <zufichris@gmail.com>
2025-03-21 20:13:20 +0400

    ?? components/auth/

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,49 +1,46 @@
 import { env } from "@/config/env";
 import { Currencies } from "@/types/pricing";
 
-export async function request<T>(path: string, init?: RequestInit, baseUrl?: string): Promise<T> {
-    try {
-        const url = new URL(path, baseUrl ?? env.apiUrl).toString();
-        const res = await fetch(url, {
-            headers: {
-                "Content-Type": "application/json",
-            },
-            method: 'GET',
-            ...(init ?? {}),
-        });
-        console.log(res)
-
-        if (!res.ok) {
-            throw new Error(`Request failed with status ${res.status}`);
-        }
-        const result: T = await res.json();
-        return result;
-    } catch (error: unknown) {
-        throw error;
-    }
+export async function request<T>(path: string, init?: RequestInit, baseUrl: string | undefined = env.apiUrl): Promise<T> {
+  try {
+    const url = new URL(path, baseUrl).toString();
+    const res = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-device-name": "Itel",
+        "x-device-location": "Obang",
+      },
+      method: 'GET',
+      ...(init ?? {}),
+    });
+    const result: T = await res.json();
+    return result;
+  } catch (error: unknown) {
+    throw error;
+  }
 }
 
-export  function generateSlug(name: string) {
-    const slug = name
-      .toLowerCase()
-      .replace(/[^\w\s-]/g, "")
-      .replace(/\s+/g, "-")
- return slug
-    }
+export function generateSlug(name: string) {
+  const slug = name
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+  return slug
+}
 
-    export function getCurrencySymbol(currency:string) {
-        switch (currency) {
-          case Currencies.USD:
-            return "$"
-          case Currencies.EUR:
-            return "€"
-          case Currencies.ETH:
-            return "Ξ"
-          case Currencies.TRON:
-            return "TRX"
-          case Currencies.AED:
-            return "د.إ"
-          default:
-            return ""
-        }
-      }
+export function getCurrencySymbol(currency: string) {
+  switch (currency) {
+    case Currencies.USD:
+      return "$"
+    case Currencies.EUR:
+      return "€"
+    case Currencies.ETH:
+      return "Ξ"
+    case Currencies.TRON:
+      return "TRX"
+    case Currencies.AED:
+      return "د.إ"
+    default:
+      return ""
+  }
+}
