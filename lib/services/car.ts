67b89df48214e2e7a5796bd48ff8f801e9de9458
zufichris <zufichris@gmail.com>
2025-03-24 17:09:03 +0400

    Commit changes for lib/services/car.ts

--- lib/services/car.ts
+++ lib/services/car.ts
@@ -1,8 +1,7 @@
 import { generateFakeCar } from "@/mock.data";
 import { GetCarsFilter, GetCarsResponse, ICar } from "@/types/car";
-import { BaseService, IResponse} from "@/types/shared";
-import { promisify } from "util";
-
+import { BaseService, IResponse, IResponsePaginated } from "@/types/shared";
+import { request } from "@/utils/functions";
 export class CarService extends BaseService {
     car: ICar = generateFakeCar()
     cars: ICar[] = Array.from({ length: 20 }).map(x => generateFakeCar())
@@ -10,35 +9,27 @@ export class CarService extends BaseService {
         super()
     }
 
-    async getCarById(id: number): Promise<IResponse<ICar>> {
+    async getCarById(id: string): Promise<IResponse<ICar>> {
         try {
-            // const res = await request<IResponse<ICar>>(`${this.basePath}/${id}`)
-            // return res
-            await promisify(setTimeout)(1900)
-            return ({
-                success: true,
-                message: "Car Retrieved",
-                data: this.car
-            })
+            console.log(this.car)
+            const res = await request<IResponse<ICar>>(`${this.basePath}/${id}`)
+            return res
         } catch (error) {
             return this.handleError({})
         }
     }
     async getCars(filters?: GetCarsFilter): Promise<GetCarsResponse> {
         try {
-            // const res = await request<IResponsePaginated<ICar>>(`${this.basePath}?${query}`)
-            // return res
-            await promisify(setTimeout)(100)
-
+            const stringifiedFilters = Object.entries(filters ?? {}).reduce((acc, [key, value]) => ({
+                ...acc,
+                [key]: String(value)
+            }), {})
+            const query = new URLSearchParams(stringifiedFilters).toString()
+            const res = await request<IResponsePaginated<ICar>>(`${this.basePath}?${query}`)
             return ({
-                success: true,
-                message: "Cars Retrieved",
-                data: this.cars,
-                limit: this.cars.length,
-                page: 1,
-                total: this.cars.length,
-                activeCount: this.cars.filter(x => x.isActive).length,
-                deletedCount: this.cars.filter(x => x.isDeleted).length,
+                ...res,
+                activeCount: res.success ? res.data.filter(x => x.isActive).length : 0,
+                deletedCount: res.success ? res.data.filter(x => x.isDeleted).length : 0,
             })
         } catch (error) {
             return this.handleError({})
@@ -47,4 +38,4 @@ export class CarService extends BaseService {
 
 }
 
-export const carsService = new CarService('/cars')
+export const carsService = new CarService('/api/v1/cars')
