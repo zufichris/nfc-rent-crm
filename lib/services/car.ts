2e4319fbc40fb1c78b6bb4632a1a57647a19c8dc
zufichris <zufichris@gmail.com>
2025-03-29 14:30:12 +0400

    Update: lib/services/car.ts

--- lib/services/car.ts
+++ lib/services/car.ts
@@ -4,20 +4,47 @@ import { GetCarsFilter, GetCarsResponse, ICar } from "@/types/car";
 import { BaseService, IResponse, IResponsePaginated } from "@/types/shared";
 import { request } from "@/utils/functions";
 export class CarService extends BaseService {
-    car: ICar = generateFakeCar()
-    cars: ICar[] = Array.from({ length: 20 }).map(x => generateFakeCar())
     constructor(private readonly basePath: string) {
         super()
     }
-    validateCarFormData(data: unknown) {
-        const valid = CarFormSchema.safeParse(data)
-        if (valid.success) {
-            return { data: valid.data, error: undefined }
+    async create(data: Record<string, unknown>) {
+        try {
+            const valid = this.validate(CarFormSchema,data)
+            if (!valid.error) {
+                const res = await request<IResponse<ICar>>(`${this.basePath}`, {
+                    method: "POST",
+                    body: JSON.stringify(data)
+                })
+                return res
+            } else {
+                return this.handleError({
+                    message: valid.error,
+                    status: 400
+                })
+            }
+        } catch (error) {
+            return this.handleError({})
         }
-        return { error: valid.error.message }
-
     }
-
+    async update(data: Record<string, unknown>) {
+        try {
+            const valid = this.validate(CarFormSchema,data)
+            if (!valid.error) {
+                const res = await request<IResponse<ICar>>(`${this.basePath}`, {
+                    method: "PUT",
+                    body: JSON.stringify(data)
+                })
+                return res
+            } else {
+                return this.handleError({
+                    message: valid.error,
+                    status: 400
+                })
+            }
+        } catch (error) {
+            return this.handleError({})
+        }
+    }
     async getCarById(id: string): Promise<IResponse<ICar>> {
         try {
             const res = await request<IResponse<ICar>>(`${this.basePath}/${id}`)
@@ -39,44 +66,6 @@ export class CarService extends BaseService {
                 activeCount: res.success ? res.data.filter(x => x.isActive).length : 0,
                 deletedCount: res.success ? res.data.filter(x => x.isDeleted).length : 0,
             })
-        } catch (error) {
-            return this.handleError({})
-        }
-    }
-    async create(data: Record<string, unknown>) {
-        try {
-            const valid = this.validateCarFormData(data)
-            if (!valid.error) {
-                const res = await request<IResponse<ICar>>(`${this.basePath}`, {
-                    method: "POST",
-                    body: JSON.stringify(data)
-                })
-                return res
-            } else {
-                return this.handleError({
-                    message: valid.error,
-                    status: 400
-                })
-            }
-        } catch (error) {
-            return this.handleError({})
-        }
-    }
-    async update(data: Record<string, unknown>) {
-        try {
-            const valid = this.validateCarFormData(data)
-            if (!valid.error) {
-                const res = await request<IResponse<ICar>>(`${this.basePath}`, {
-                    method: "PUT",
-                    body: JSON.stringify(data)
-                })
-                return res
-            } else {
-                return this.handleError({
-                    message: valid.error,
-                    status: 400
-                })
-            }
         } catch (error) {
             return this.handleError({})
         }
