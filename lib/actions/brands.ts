4e11756b5f11a96afe2bcdaed625d2c682eb9dd3
zufichris <zufichris@gmail.com>
2025-03-29 14:29:58 +0400

    Update: lib/actions/brands.ts

--- lib/actions/brands.ts
+++ lib/actions/brands.ts
@@ -1,7 +1,8 @@
 'use server'
-
 import { GetBrandsFilters, GetBrandsResponse, IBrand } from "@/types/brand"
 import { brandsService } from "../services/brand"
+import { BrandFormSchema } from "@/components/brands/brands-form/schema"
+import { z } from "zod"
 
 export async function getBrandById(id: string) {
     const res = await brandsService.getBrandById(id)
@@ -16,11 +17,11 @@ export async function deleteBrand(id: string) {
     const data = await brandsService.getBrandById(id)
     return data
 }
-export async function updateBrand(brand: Partial<IBrand>) {
-    const data = await brandsService.getBrandById(brand.id!)
-    return data
+export async function updateBrand(brandId: string, data: Partial<IBrand>) {
+    const res = await brandsService.update(brandId, data)
+    return res
 }
-export async function createBrand(brand: Partial<IBrand>) {
-    const data = await brandsService.getBrandById(brand.id!)
-    return data
+export async function createBrand(data: z.infer<typeof BrandFormSchema>) {
+    const res = await brandsService.create(data)
+    return res
 }
