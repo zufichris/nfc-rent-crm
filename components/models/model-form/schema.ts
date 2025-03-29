5a7d4f6eeaaf4c5184e8f002452e09bc88e3d5af
zufichris <zufichris@gmail.com>
2025-03-29 14:29:29 +0400

    Update: app/[locale]/fleet-management/brands/[id]/edit/loading.tsx

---
+++ components/models/model-form/schema.ts
@@ -0,0 +1,43 @@
+import { Languages } from "@/types/language";
+import { IModel } from "@/types/model";
+import { z } from "zod";
+
+
+const CarModelTranslationsSchema = z.array(
+    z.object({
+        name: z.string({
+            message: "Name is required"
+        }),
+        shortDescription: z.string().optional(),
+        description: z.string().optional(),
+        metaTitle: z.string().optional(),
+        metaDescription: z.string().optional(),
+        metaTags: z.array(z.string()).optional().default([])
+    })
+).nonempty()
+
+const CarModelFormSchema = z.object({
+    brandId: z.string({
+        message: "Brand ID is required",
+    }),
+    translations: CarModelTranslationsSchema
+})
+
+function getModelFormDefaultValues(model?: IModel): z.infer<typeof CarModelFormSchema> {
+
+    return ({
+        brandId: model?.brand?.id ?? "",
+        translations: Languages.map(l => ({
+            name: model?.name ?? "",
+            description: model?.description ?? "",
+            shortDescription: model?.shortDescription ?? "",
+            metaDescription: model?.metadata?.description ?? "",
+            metaTitle: model?.metadata?.title ?? "",
+            metaTags: model?.metadata?.tags ?? []
+        })) as z.infer<typeof CarModelTranslationsSchema>
+
+    })
+
+}
+
+export { CarModelFormSchema, CarModelTranslationsSchema, getModelFormDefaultValues }
