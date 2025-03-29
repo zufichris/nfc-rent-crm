4634e92f6c53998271d8ae2e3632f945607a7f7f
zufichris <zufichris@gmail.com>
2025-03-29 14:30:19 +0400

    Update: lib/services/model.ts

--- lib/services/model.ts
+++ lib/services/model.ts
@@ -1,12 +1,63 @@
+import { CarModelFormSchema } from "@/components/models/model-form/schema";
 import { GetModelsFilters, GetModelsResponse, IModel } from "@/types/model";
 import { BaseService, IResponse, IResponsePaginated } from "@/types/shared";
 import { request, stringifyFilters } from "@/utils/functions";
+import { z } from "zod";
 
 export class ModelsService extends BaseService {
     constructor(private readonly basePath: string) {
         super()
     }
-
+    async create(data: Record<string, unknown>) {
+        try {
+            const valid = this.validate<z.infer<typeof CarModelFormSchema>>(CarModelFormSchema, data)
+            if (!valid.error) {
+                if (valid.data?.translations?.length) {
+                    valid.data.translations = valid.data?.translations.map(t => ({
+                        ...t,
+                        metaTags: t.metaTags.join(",")
+                    })) as any
+                }
+                const res = await request<IResponse<IModel>>(`${this.basePath}`, {
+                    method: this.methods.POST,
+                    body: JSON.stringify(valid.data)
+                })
+                return res
+            } else {
+                return this.handleError({
+                    message: valid.error,
+                    status: 400
+                })
+            }
+        } catch (error) {
+            return this.handleError({})
+        }
+    }
+    async update(modelId: string, data: Record<string, unknown>) {
+        try {
+            const valid = this.validate<z.infer<typeof CarModelFormSchema>>(CarModelFormSchema, data)
+            if (!valid.error) {
+                if (valid.data?.translations?.length) {
+                    valid.data.translations = valid.data?.translations.map(t => ({
+                        ...t,
+                        metaTags: t.metaTags.join(",")
+                    })) as any
+                }
+                const res = await request<IResponse<IModel>>(`${this.basePath}/${modelId}`, {
+                    method: this.methods.PATCH,
+                    body: JSON.stringify(valid.data)
+                })
+                return res
+            } else {
+                return this.handleError({
+                    message: valid.error,
+                    status: 400
+                })
+            }
+        } catch (error) {
+            return this.handleError({})
+        }
+    }
     async getModelById(id: string): Promise<IResponse<IModel>> {
         try {
             const res = await request<IResponse<IModel>>(`${this.basePath}/${id}`)
